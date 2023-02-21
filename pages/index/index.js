48a8fa07e7734c85df5895709574072e4235b550
guoaihua <308232015@qq.com>
2023-02-21 23:57:42 +0800

    feat: use online api

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,7 +14,7 @@ const healthList = [
 
 Page({
   data: {
-    healthInfo: healthList[0]
+    healthInfo: ''
   },
   // 事件处理函数
   bindViewTap() {
@@ -22,8 +22,18 @@ Page({
       url: '../logs/logs'
     })
   },
+  onReady(){
+      const that = this;
+      wx.request({
+        url: 'https://ziming.online/miniapp/getHealthTip',
+        success(res){
+            that.setData({
+                healthInfo: res.data
+            })
+        }
+      })
+  },
   onLoad() {
-    console.log(this.data.healthInfo)
     if (wx.getUserProfile) {
       this.setData({
         canIUseGetUserProfile: true
@@ -35,7 +45,6 @@ Page({
     wx.getUserProfile({
       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
-        console.log(res)
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
@@ -45,7 +54,6 @@ Page({
   },
   getUserInfo(e) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
